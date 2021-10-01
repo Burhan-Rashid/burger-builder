3ee4c296cc61873d346dda7f0e55d67fc07876c8
Burhan-Rashid <burhanuz30@gmail.com>
2021-10-01 22:00:28 +0530

    Modified Navbar

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./Navbar.css"
+import { BiMenu } from "react-icons/bi"
 
 function Navbar() {
     return (
         <div className="Navbar">
-            <p>Burger Builders</p>
-            <p>Orders</p>
+            <div className="Navbar__icon">
+                <BiMenu size={38} color="white" />
+            </div>
+            <div className="Navbar__right">
+                <p>Burger Builders</p>
+                <p>Orders</p>
+            </div>
         </div>
     )
 }
