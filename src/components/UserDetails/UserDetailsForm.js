80a4d39235f720fdcd5ab259cc27ff8f2d01bce1
Burhan-Rashid <burhanuz30@gmail.com>
2021-10-04 12:35:44 +0530

    added validation in user details form

--- src/components/UserDetails/UserDetailsForm.js
+++ src/components/UserDetails/UserDetailsForm.js
@@ -4,27 +4,88 @@ import "./UserDetailsForm.css"
 
 function UserDetailsForm() {
 
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [address, setAddress] = React.useState("");
-    const [city, setCity] = React.useState("");
-    const [phone, setPhone] = React.useState("");
-    const [deliveryType, setDeliveryType] = React.useState("");
+    const [name, setName] = React.useState({ value: "", valid: true });
+    const [email, setEmail] = React.useState({ value: "", valid: true });
+    const [address, setAddress] = React.useState({ value: "", valid: true });
+    const [city, setCity] = React.useState({ value: "", valid: true });
+    const [phone, setPhone] = React.useState({ value: "", valid: true });
+    const [deliveryType, setDeliveryType] = React.useState({ value: "", valid: true });
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(deliveryType);
+        // console.log(deliveryType);
+    }
+
+    const checkValidity = (func, value) => {
+        let valid = true;
+        if (func === setName && value.trim().length < 5) {
+            valid = false;
+        }
+        else if (func === setEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            valid = pattern.test(value);
+        }
+        else if (func === setPhone) {
+            const pattern = /^\d+$/;
+            valid = pattern.test(value) && value.length === 10;
+        }
+        else if (func === setCity && value.trim().length < 5) {
+            valid = false;
+        }
+        else if (func === setAddress && value.trim().length < 5) {
+            valid = false;
+        }
+        return valid;
+    }
+
+    const handleChange = (e, func) => {
+        let valid = checkValidity(func, e.target.value);
+        func({ value: e.target.value, valid: valid });
     }
 
     return (
         <div className="UserDetailsForm">
             <strong>Enter your Details:</strong>
             <form onSubmit={handleSubmit} className="form">
-                <input type="text" placeholder="Name" required value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="text" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="text" placeholder="Address" required value={address} onChange={(e) => setAddress(e.target.value)} />
-                <input type="text" placeholder="City" required value={city} onChange={(e) => setCity(e.target.value)} />
-                <input type="number" placeholder="Phone Number" required value={phone} onChange={(e) => setPhone(e.target.value)} />
+                <input
+                    className={name.valid ? "UserDetailsForm__input" : "UserDetailsForm__input invalid"}
+                    type="text"
+                    placeholder="Name"
+                    required
+                    value={name.value}
+                    onChange={(e) => handleChange(e, setName)}
+                />
+                <input
+                    className={email.valid ? "UserDetailsForm__input" : "UserDetailsForm__input invalid"}
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email.value}
+                    onChange={(e) => handleChange(e, setEmail)}
+                />
+                <input
+                    className={address.valid ? "UserDetailsForm__input" : "UserDetailsForm__input invalid"}
+                    type="text"
+                    placeholder="Address"
+                    required
+                    value={address.value}
+                    onChange={(e) => handleChange(e, setAddress)}
+                />
+                <input className={city.valid ? "UserDetailsForm__input" : "UserDetailsForm__input invalid"}
+                    type="text"
+                    placeholder="City"
+                    required
+                    value={city.value}
+                    onChange={(e) => handleChange(e, setCity)}
+                />
+                <input
+                    className={phone.valid ? "UserDetailsForm__input" : "UserDetailsForm__input invalid"}
+                    type="number"
+                    placeholder="Phone Number"
+                    required
+                    value={phone.value}
+                    onChange={(e) => handleChange(e, setPhone)}
+                />
 
                 <select onChange={(e) => setDeliveryType(e.target.value)}>
                     <option default disabled selected>Select Delivery Type</option>
