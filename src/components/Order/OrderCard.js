71f240687741e3447e96ddde2aacc055d5cedf98
Burhan-Rashid <burhanuz30@gmail.com>
2021-10-11 21:22:12 +0530

    Fixed alignment and date formating in orders page

--- src/components/Order/OrderCard.js
+++ src/components/Order/OrderCard.js
@@ -2,11 +2,14 @@ import React from 'react'
 import "./OrderCard.css"
 
 function OrderCard({ order }) {
+
+    let date = new Date(order.orderTime);
+
     return (
         <div className="OrderCard">
             <h3>Order Details</h3>
             <p>Cheese:({order.ingredients.cheese}), Meat:({order.ingredients.meat}), Salad:({order.ingredients.salad}), Bacon:({order.ingredients.bacon})</p>
-            <p>Date: <strong> {order.orderTime}</strong> </p>
+            <p>Date: <strong> {date.toLocaleString()}</strong> </p>
             <p>Delivery Mode: <strong> {order.deliveryType}</strong></p>
             <p>Price: <strong> {order.price}$</strong></p>
         </div>
