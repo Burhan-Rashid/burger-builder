66d3de6c23ca3be3887477125158c724299ea7da
Burhan-Rashid <burhanuz30@gmail.com>
2021-10-17 22:02:56 +0530

    Added mobile responsiveness to order summary modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 import { withRouter } from 'react-router'
 import Backdrop from '../Backdrop/Backdrop'
 import Button from '../UI/Button/Button'
